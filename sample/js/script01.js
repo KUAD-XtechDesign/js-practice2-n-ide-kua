3b39d2405caa09a77bb512810961a673f34ba81d
ide-natsuki <n-ide6270@st.kyoto-art.ac.jp>
2021-10-15 14:39:21 +0900

    Update script01.js

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,10 +1,37 @@
 $(function(){
 
-　 function sayhello(){
-    $("content").text("こんにちは!")
+　 function sayHello(){
+    $("content").text("こんにちは!");
+   }
+
+   function saySomething(txt){
+    $("content").text(txt);
+   }
+
+   function samTwo(arg1,arg2){
+    $("content").text(txt);
    }
 
  $("#button01").on("click",function(){
-     sayhello();
+     sayHello();
  })
+
+ $("#button02").on("click",function(){
+    saySomething();
+ })
+
+ $("#button03").on("click",function(){
+    samTwo(txt1,txt2);
+ })
+
+ $("#button04").on("click",function(){
+    samTwo(num1,num2);
+ })
+
+ $("#button05").on("click",function(){
+    samTwo(txt1,txt2);
+    samTwo(num1,num2);
+ })
+
+
 })
